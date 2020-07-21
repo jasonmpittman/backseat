d4b7b35d265a9add278384767d01df4489c668cc
Kevin Rubin <kar3154@gmail.com>
2020-07-21 15:36:43 -0400

    Added comments to run_command function

--- agent.py
+++ agent.py
@@ -1,11 +1,21 @@
-import os
+import subprocess
+
+import sys
+
 
 class Agent:
 	def __init__(self):
-		self.run_command("ls -al")
+		pass
 
+	'''
+	This function takes in a command then executes the command.
+	The results are then returned as as string.
+	'''
 	def run_command(self, command):
-		test = os.system(command)
-		print(test)
+		command_list = command.split(" ")
+		subprocess_result = subprocess.run(command_list, stdout=subprocess.PIPE)
+		string_result = subprocess_result.stdout.decode("utf-8")
+		return string_result
 
 A = Agent()
+A.run_command("ls -l")
