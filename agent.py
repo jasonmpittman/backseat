ad152504f388a2b62fa3c9d8c81d2458d7550f7c
Kevin Rubin <kar3154@gmail.com>
2020-07-23 15:34:52 -0400

    added the ability for the list of out of date macOS software to be listed

--- agent.py
+++ agent.py
@@ -16,21 +16,26 @@ class Agent:
 		else:
 			return plat
 
-
 	'''
 	This function takes in a command then executes the command.
 	The results are then returned as as string.
 	'''
 	def run_command(self, command):
 		command_list = command.split(" ")
-		subprocess_result = subprocess.run(command_list, capture_output=True)
+		subprocess_result = subprocess.run(command_list, capture_output=True, check=True)
 		string_result = subprocess_result.stdout.decode("utf-8")
+		string_error = subprocess_result.stderr.decode("utf-8")
 		string_result =  string_result[:-1]
-		return string_result
+		return string_result, string_error
 
+	def mac_get_updates_list(self):
+		brew_command = 'brew outdated'
+		macOS_command = 'softwareupdate -l'
+		macOS_command_res, macOS_command_err = self.run_command(macOS_command)
+		brew_command_res, brew_command_err = self.run_command(brew_command)
+		return macOS_command_res +"\n" + macOS_command_err + brew_command_res + brew_command_err
 
 if __name__ == "__main__":
 	A = Agent()
-	res = A.run_command("ls -l")
-	print(res)
 	print(A._platform)
+	print(A.mac_get_updates_list())
