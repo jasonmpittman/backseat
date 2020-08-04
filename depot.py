5798beabd4d968cae6c9b99e476a72cb311a7b4b
Kevin Rubin <kar3154@gmail.com>
2020-08-04 15:25:59 -0400

    created Depot class with basic functionality

--- depot.py
+++ depot.py
@@ -4,7 +4,18 @@ class DepotItem:
 	def __init__(self, command, command_id):
 		# self._host = host
 		self._command = command
-		self._command_id = command_id
+		self.command_id = command_id
 		self._done = False
 		self._stdout = ""
 		self._exit_code = None
+
+class Depot:
+	def __init__(self, host):
+		self._host = host
+		self._depot_active_list = []
+		# self._depot_completed_list = []
+
+	def get_by_id(self, id):
+		for command_object in self._depot_active_list:
+			if command_object.command_id == id:
+				return command_object
