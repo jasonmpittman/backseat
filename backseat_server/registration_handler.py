02eea2784c73d669d9639281196e10155d9ad9b3
Kevin Rubin <kar3154@gmail.com>
2020-08-18 11:04:54 -0400

    Added more to RestrationHandler

--- backseat_server/registration_handler.py
+++ backseat_server/registration_handler.py
@@ -10,6 +10,11 @@ class RegistrationHandler:
 		self._host_list.append({"host": host, "OS": OS})
 
 
+	def get_registration_info(self):
+		with open("host.config", "r") as F:
+			# Add the next portion to this
+
+
 	def get_hosts(self, host_type):
 		res = []
 		for host in self._host_list:
@@ -23,9 +28,11 @@ class RegistrationHandler:
 	def get_ubuntu_hosts(self):
 		return self.get_hosts("Ubuntu")
 
-	def get_centOS_hosts(self):
-		return self.get_hosts("CentOS")
-		# This may need to be cut into more than 1 pieces
+	def get_centOS_7_hosts(self):
+		return self.get_hosts("CentOS 7")
+
+	def get_centOS_8_hosts(self):
+		return self.get_hosts("CentOS 8")
 
 	def get_fedora_hosts(self):
 		return self.get_host("Fedora")
