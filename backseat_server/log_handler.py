733816b80a47d2dd7e66895927f6bd84b8886e64
Kevin Rubin <kar3154@gmail.com>
2020-08-18 09:18:50 -0400

    Added basics of the logging system

--- backseat_server/log_handler.py
+++ backseat_server/log_handler.py
@@ -4,5 +4,19 @@ Handles all the logging.
 import logging
 
 class LogHandler:
-	def __init__(self):
-		pass
+	def __init__(self, name):
+		self.logger = logging.getLogger(name)
+		self.logger.setLevel(logging.INFO)
+		self.file_handler = logging.FileHandler("Logfile.log")
+		self.formatter = logging.Formatter("%(asctime)s: %(levelname)s: %(name)s:%(message)s")
+		self.file_handler.setFormatter(self.formatter)
+		self.logger.addHandler(self.file_handler)
+
+	def info(self, class_name, fuction_name, message):
+		self.logger.info(f"{class_name}.{function_name} - {message}")
+
+	def warning(self, class_name, function_name, message):
+		self.logger.warning(f"{class_name}.{function_name} - {message}")
+
+	def error(self, class_name, function_name, message):
+		self.logger.error(f"{class_name}.{function_name} - {message}")
