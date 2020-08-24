ef3c6cda43a86191e95191eab196d2a54155f3e8
Kevin Rubin <kar3154@gmail.com>
2020-08-24 10:50:34 -0400

    Added feature to handle repeat data

--- backseat_server/patch_handler.py
+++ backseat_server/patch_handler.py
@@ -1,7 +1,5 @@
 '''
 Handles everything involved with patches, gets them, and creates installation commands.
-
-Interfaces with websites that have lists of security issues in software and OS's.
 '''
 
 import xmltodict
@@ -10,48 +8,7 @@ class PatchHandler:
 	def __init__(self, depot_list=None):
 		if depot_list == None:
 			print("depot_list == None")
-		self._depot_list = depot_list
-		with open('backseat_server/allitems.xml') as fd:
-			self._xml_doc = xmltodict.parse(fd.read())
+
 
 if __name__ == "__main__":
 	PH = PatchHandler()
-	print("starting to print")
-	# print(PH._xml_doc["cve"]["item"])
-	listy = []
-	for item in PH._xml_doc["cve"]["item"]:
-		print("---")
-		name = item["@name"]
-		seq = item["@seq"]
-		type = item["@type"]
-		status = item["status"]
-		try:
-			phase = item["phase"]
-		except:
-			phase = "Failed"
-
-		desc = item["desc"]
-		try:
-			refs = item["refs"]["ref"]
-		except:
-			print("candidate: " + str(item["refs"]))
-			refs = None
-
-		print("name: " + name)
-		print("seq: " + seq)
-		print("type: " + type)
-		print("status: " + status)
-		print("phase: " + str(phase))
-		print("desc:" + desc)
-		print("refs: ")
-		print(refs)
-		try:
-			for ref in refs:
-				print("\t" + ref["@source"])
-				print("\t" + ref["@url"])
-				print("\t" + ref["#text"])
-				print("-")
-		except:
-			print("\tNone")
-
-	print("finnishing printing")
--- backseat_server/registration_handler.py
+++ backseat_server/registration_handler.py
@@ -5,6 +5,7 @@ This portion deals with the computers that are registered.
 class RegistrationHandler:
 	def __init__(self):
 		self._host_list = []
+		self.get_registration_info()
 
 	def add(self, host, OS):
 		self._host_list.append({"host": host, "OS": OS})
@@ -13,6 +14,29 @@ class RegistrationHandler:
 	def get_registration_info(self):
 		with open("host.config", "r") as F:
 			# Add the next portion to this
+			lines = F.readlines()
+			for line in lines:
+				line = line.replace("\n", "")
+				line = line.replace(" ", "")
+				host, OS = line.split(",")
+				hosts = [h["host"] for h in self._host_list]
+				if host not in hosts:
+					self.add(host, OS)
+				else:
+					print(f"repeat host [{host}] is not being added to self._host_list")
+
+	def register_new_host(self, host, OS):
+		current_hosts = []
+		with open("host.config", "r") as F:
+			lines = F.readlines()
+			for line in lines:
+				line = line.replace("\n", "")
+				line = line.replace(" ", "")
+				host, OS = line.split(",")
+				current_hosts.append(host)
+
+		with open("host.config", "a") as F:
+			F.write(f"{host}, {OS}\n")
 
 
 	def get_hosts(self, host_type):
@@ -36,3 +60,15 @@ class RegistrationHandler:
 
 	def get_fedora_hosts(self):
 		return self.get_host("Fedora")
+
+	def print_host_list(self):
+		for host in self._host_list:
+			print(f"{host['host']}: {host['OS']}")
+
+
+
+
+if __name__ == "__main__":
+	RH = RegistrationHandler()
+	RH.print_host_list()
+	RH.register_new_host("132.456.789.1011", "Ubuntu")
