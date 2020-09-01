86cd4427b9ff3c7ad3fa41698c453309af07e3f4
Kevin Rubin <kar3154@gmail.com>
2020-09-01 09:58:06 -0400

    Added documentation to the LogHandler

--- shared/log_handler.py
+++ shared/log_handler.py
@@ -5,6 +5,11 @@ import logging
 
 class LogHandler:
 	def __init__(self, class_name):
+		'''
+		Input: class_name (str): name of the class that the logger is in
+
+		Initializes all important parts of the LogHandler. Sets up formatting and logger parts.
+		'''
 		self._class_name = class_name
 		self.logger = logging.getLogger(self._class_name)
 		self.logger.setLevel(logging.INFO)
@@ -14,12 +19,30 @@ class LogHandler:
 		self.logger.addHandler(self.file_handler)
 
 	def info(self, function_name, message):
+		'''
+		Input:
+			function_name (str): name of the function that is running
+			message (str): important information that needs to be logged
+		Writes an info message to the log file.
+		'''
 		self.logger.info(f"{self._class_name}.{function_name} - {message}")
 
 	def warning(self, function_name, message):
+		'''
+		Input:
+			function_name (str): name of the function that is running
+			message (str): important information that needs to be logged
+		Writes a warning message to the log file.
+		'''
 		self.logger.warning(f"{self._class_name}.{function_name} - {message}")
 
 	def error(self, function_name, message):
+		'''
+		Input:
+			function_name (str): name of the function that is running
+			message (str): important information that needs to be logged
+		Writes an error message to the log file.
+		'''
 		self.logger.error(f"{self._class_name}.{function_name} - {message}")
 
 if __name__ == "__main__":
