4a59cd6ccb4521f29ea14e7c949c6767328d3ba3
Kevin Rubin <kar3154@gmail.com>
2020-10-05 11:01:02 -0400

    Added documentation to AccountHandler

--- backseat_server/account_handler.py
+++ backseat_server/account_handler.py
@@ -8,48 +8,100 @@ Handles the user accounts that need to access the server through the UI.
 
 import hashlib
 
+from shared import log_handler
+
 class AccountHandler:
+	"""
+	Handles the user accounts associated with those who will use the server
+	UI.
+
+	Attributes
+	----------
+	_accounts : list
+	"""
 	def __init__(self):
-		f = open("accounts.config")
-		file_lines = f.readlines()
+		"""
+		This function sets up the _accounts list by getting the information
+		from the accounts.config file.
 
-		f.close()
-		self._accounts = []
-		for file_line in file_lines:
-			file_line = file_line.strip("\n")
-			usr, pword = file_line.split(",")
-			self._accounts.append({"username": usr, "password": pword})
+		Parameters
+		----------
 
-		print(self._accounts)
+		"""
+		self._log = log_handler.LogHandler(self.__class__.__name__)
+		try:
+			f = open("accounts.config")
+			file_lines = f.readlines()
+			f.close()
+			self._accounts = []
+			for file_line in file_lines:
+				file_line = file_line.strip("\n")
+				usr, pword = file_line.split(",")
+				self._accounts.append({"username": usr, "password": pword})
+			self._log.info("__init__", "Accounts loaded from accounts.config")
+		except:
+			self._log.error("__init__", "Accounts could not be loaded from accounts.config - returned None")
+			return None
+
 
 	def _hash(self, str_obj):
-		return hashlib.sha512(f"{str_obj}".encode()).hexdigest()
+		"""
+		This function returns a sha512 hash of what ever string is passed in.
 
+		Parameters
+		----------
+		str_obj : str
+		"""
+		try:
+			return hashlib.sha512(f"{str_obj}".encode()).hexdigest()
+		except:
+			self._log.error("_hash", "Hashing failed - returned None")
+			return None
 
 	def verify(self, username, password):
+		"""
+		This checks if a given username password pair has an account. It hashes
+		the password and checks it against the stored hashed password.
+
+		Parameters
+		----------
+		username : str
+		password : str
+		"""
 		for account in self._accounts:
 			if account["username"] == username:
 				if self._hash(password) == account["password"]:
+					self._log.info("verify", "Username and password correct - Returned True")
 					return True
 				else:
-					print("Password or username incorrect")
+					self._log.info("verify", "Password or username incorrect - Returned False")
 					return False
-		print("Password or username incorrect")
+		self._log("verify", "Password or username incorrect - Returned False")
 		return False
 
 	def add_account(self, username, password):
+		"""
+		Adds an account to the accounts.config file after checking to ensure
+		no duplicate usernames.
+
+		Parameters
+		----------
+		username : str
+		password : str
+		"""
 		for a in self._accounts:
 			if a["username"] == username:
-				print("Username already in use - Returned None")
+				self._log.error("add_account","Username already in use - Returned None")
 				return None
 		account = {"username": username, "password": self._hash(password)}
 		self._accounts.append(account)
 		file = open("accounts.config", "w")
 		for acc in self._accounts:
 			file.write(f"{acc['username']},{acc['password']}\n")
+
 		file.close()
+		self._log.info("add_account", "Account information has been written to accounts.config")
 
 
 if __name__ == "__main__":
-
 	AH = AccountHandler()
