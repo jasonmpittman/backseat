de4403be00b3b9237162084e49faa1b5dda1b071
Kevin Rubin <kar3154@gmail.com>
2020-10-06 10:19:45 -0400

    Added commandline arguements to both server and endpoint

--- endpoint.py
+++ endpoint.py
@@ -3,8 +3,16 @@
 # socket_handler = tcp_socket_handler.TcpSocketHandler()
 
 # client_socket = socket_handler.create_client_socket_connect("localhost", 9999)
-
+import sys
 from backseat_endpoint import endpoint
 
-E = endpoint.Endpoint("localhost", 9999, "private.pem", "public.pem")
-E.operate()
+if __name__ == "__main__":
+	if len(sys.argv) != 5:
+		print(f"Needs 4 command line arguements [server_ip, server_port, my_private, their_public], {len(sys.argv)-1} provided")
+		exit()
+	ip = sys.argv[1]
+	port = int(sys.argv[2])
+	my_private = sys.argv[3]
+	their_public = sys.argv[4]
+	E = endpoint.Endpoint(ip, port, my_private, their_public)
+	E.operate()
--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from backseat_server import server_loop
 
-
+import sys
 if __name__ == "__main__":
-	SL = server_loop.ServerLoop("localhost", 9999, 10)
+	if len(sys.argv) != 3:
+		print(f"Needs 2 command line arguements [server_ip and server_port], {len(sys.argv)-1} provided")
+		exit()
+	ip = sys.argv[1]
+	port = int(sys.argv[2])
+	SL = server_loop.ServerLoop(ip, port, 10)
 	SL.server_loop()
