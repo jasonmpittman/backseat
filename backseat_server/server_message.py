712696bf23de761d5ed7cb643e35ff9c350516c8
Kevin Rubin <kar3154@gmail.com>
2020-11-04 13:23:27 -0500

    Added documentation to server_message.py

--- backseat_server/server_message.py
+++ backseat_server/server_message.py
@@ -1,14 +1,53 @@
 import json
 
 class ServerMessage:
+	"""
+	Creates the messages that the server sends to the endpoints.
+
+	Attributes
+	----------
+	_not_ready : bool
+	_command : str
+	_sudo : bool
+	_password : str
+	_sequence : int
+	_depot_items : int
+	_command_id : int
+	"""
 	def __init__(self):
 		pass
 
 	def create_msg(self, not_ready, command, sudo, password, sequence, depot_items, command_id):
+		"""
+		Sets the values of the variables that belong to the class then returns that information in json format.
+
+		Parameters
+		----------
+		not_ready : bool
+		command : str
+		sudo : bool
+		password : str
+		sequence : int
+		depot_items : int
+		command_id : int
+		"""
 		self.add_data(not_ready, command, sudo, password, sequence, depot_items, command_id)
 		return self.to_json()
 
 	def add_data(self, not_ready, command, sudo, password, sequence, depot_items, command_id=0):
+		"""
+		Sets the values of the variables that belong to the class.
+
+		Parameters
+		----------
+		not_ready : bool
+		command : str
+		sudo : bool
+		password : str
+		sequence : int
+		depot_items : int
+		command_id : int
+		"""
 		self._not_ready = not_ready
 		self._command = command
 		self._sudo = sudo
@@ -18,6 +57,12 @@ class ServerMessage:
 		self._command_id = command_id
 
 	def to_json(self):
+		"""
+		Converts the variables that belong to the class into json format then return the json str.
+
+		Parameters
+		----------
+		"""
 		dict = {"not_ready": self._not_ready, "command": self._command, "sudo": self._sudo, "password": self._password, "sequence": 0, "depot_items": self._depot_items, "command_id": self._command_id}
 		output_json = json.dumps(dict)
 		return output_json
