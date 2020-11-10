f6d155d49b59754e02b4b11aac7ec121036c8ea4
Kevin Rubin <kar3154@gmail.com>
2020-11-10 11:52:53 -0500

    Fixing time problems

--- backseat_server/server_update_handler.py
+++ backseat_server/server_update_handler.py
@@ -6,6 +6,8 @@ from shared import read_server_config
 
 import json
 
+import time
+
 # message_dict = {"type": "add", "command": "PWD", "who": ["client1_public.pem"]}
 # message_dict = {"type": "checkoff", "command_id": 3, "who": "client1_public.pem"}
 
@@ -34,6 +36,22 @@ class ServerUpdateHandler:
 		self._send_server_update(message)
 		return self._recieve_server_info()
 
+	def print_info_message(self, info_message):
+		print("--Server Status--")
+		print(f"Start Time: {self._date_time_format(info_message['start_time'])}")
+
+		print(f"Run Time: {self._elapsed_time_format(info_message['run_time'])}")
+
+		print(f"Time Since Last Successful Job: {self._elapsed_time_format(info_message['last_successful_job_time'])}")
+		print(f"Last successful job: {info_message['last_successful_job']}")
+		print(f"Last Heartbeat_time: {self._date_time_format(info_message['last_heartbeat_time'])}")
+		print(f"Time since heartbeat: {self._elapsed_time_format(info_message['time_since_heartbeat'])}")
+		print("--Depots' Info--")
+
+
+
+
+
 	def _send_server_update(self, message):
 		self._client_conn = self._socket_handler.create_client_socket_connect(self._ip, self._port)
 		self._socket_handler.send(self._client_conn, message, self._server_public_key)
@@ -44,3 +62,12 @@ class ServerUpdateHandler:
 		# return data_dict
 		data = json.loads(data)
 		return data
+
+
+	def _date_time_format(self, the_time):
+		return time.strftime("%a, %b %d, %Y %I:%M:%S %p", time.localtime(the_time))
+
+	def _elapsed_time_format(self, elapsed_time):
+		# return time.strftime('%H:%M:%S', time.gmtime(elapsed_time))
+		# e = int(time.gmtime(elapsed_time))
+		# return f"{e // 3600}:{(e % 3600 // 60)}:{e % 60}"
--- test_server_loopback.py
+++ test_server_loopback.py
@@ -1,13 +1,17 @@
 from backseat_server import server_update_handler
-
+import time
 SUH = server_update_handler.ServerUpdateHandler()
 
 
 
 
-SUH.add_command("ls", ["client1_private.pem"])
+# SUH.add_command("ls", ["client1_public.pem"])
 
 info = SUH.get_info_message()
-print("Test", info)
-print(type(info))
 print(info["depots_state"])
+print("---------")
+# SUH.override_command(2, "client1_public.pem")
+
+info = SUH.get_info_message()
+SUH.print_info_message(info)
+# print(time.strftime("%a, %b %d, %Y %I:%M:%S %p", time.localtime(info["start_time"])))
