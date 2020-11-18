b6312a1763be16ff54b673574f43e3d09d09b283
Kevin Rubin <kar3154@gmail.com>
2020-11-18 09:37:04 -0500

    Removed test.py file and added documentation to endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,15 +1,15 @@
-# from shared import tcp_socket_handler
-
-# socket_handler = tcp_socket_handler.TcpSocketHandler()
-
-# client_socket = socket_handler.create_client_socket_connect("localhost", 9999)
 import sys
+
 from backseat_endpoint import endpoint_operation
+
 from shared import read_server_config
 
 #serv_ip, serv_port, my_private_key, my_public_key, server_public_key
 
 def load_endpoint_config():
+	"""
+	This function gets the data in endpoint.config, and returns it as a tuple of values.
+	"""
 	f = open("endpoint.config", "r")
 	data = f.read()
 	list_data = data.split(" ")
@@ -24,5 +24,4 @@ if __name__ == "__main__":
 	ip, port, _, server_public_key = read_server_config.get_server_config()
 
 	E = endpoint_operation.EndpointOperation(ip, port, my_ip, my_port, my_private_key, my_public_key, server_public_key)
-	# E.operate()
 	E.operation_loop()
