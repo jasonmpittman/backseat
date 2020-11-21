affa996d35efa8f048c712a982537b66ea91d1ef
Kevin Rubin <kar3154@gmail.com>
2020-11-21 11:02:19 -0500

    Force run works

--- test_force_run.py
+++ test_force_run.py
@@ -4,3 +4,5 @@ TCP = tcp_socket_handler.TcpSocketHandler("server_private.pem", "server_public.p
 client = TCP.create_client_socket_connect("localhost", 9998)
 
 TCP.send(client, "_", "client1_public.pem")
+
+# FORCE RUN WORKS!!!
