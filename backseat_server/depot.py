f51dcce9d73f61e5a6857b96fffdb0a69c47321f
Kevin Rubin <kar3154@gmail.com>
2021-02-22 10:57:48 -0500

    Added ability to keep track of order the commands came in

--- backseat_server/depot.py
+++ backseat_server/depot.py
@@ -309,6 +309,5 @@ if __name__ == "__main__":
 	dl = DepotList()
 	dl.add_depot("localhost")
 	working_depot = dl.get_working_depot("localhost")
-	working_depot.mass_load_depot(["ls -al", "PWD"])
 
 #
