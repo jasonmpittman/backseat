0d60ca5a9519fefeefe96cafe74ef7bc2ae20a2f
Kevin Rubin <kar3154@gmail.com>
2021-03-31 09:34:41 -0400

    Refactored for simplicity

--- backseat_server/client_handler.py
+++ backseat_server/client_handler.py
@@ -35,6 +35,62 @@ class ClientHandler:
 		working_depot = self.depot_list.get_working_depot(self._key_table["client1_public.pem"])
 		working_depot.add("ls -al", 100)
 		working_depot.add("PWD", 101)
+	
+	def _server_client_handler(self, client_dict, sender_key):
+		print("SERVER SENDER KEY")
+		if client_dict["type"] == "add":
+			self._command_handler.add_command_to_specified(client_dict["command"], client_dict["who"])
+			print("### - added to depot - ###")
+			print(self.depot_list.get_depot_list_info())
+			return "depot_item_added", -1
+			
+		if client_dict["type"] == "checkoff":
+			print("In checkout code")
+			self._command_handler.checkoff_command(client_dict["who"], client_dict["command_id"])
+			return "checked_off_depot_item", -1
+
+		if client_dict["type"] == "get_server_data":
+			print("Get Server Data")
+			self._server_info.update_depots_state(self.get_depots_data())
+			output = self._server_info.to_json()
+			print("--server_info--")
+			print(output)
+			return "get_server_data", output
+		
+		if client_dict["type"] == "get_startup_data":
+			print("Get Startup Data")
+			json_static_endpoint_data = self._server_info.get_static_endpoint_data()
+			return "get_startup_data", json_static_endpoint_data
+
+		print("Recieved message from server, but 'type' is not 'add' or 'checkoff'.")
+
+		return None
+	
+	def _client_ping(self, client_dict, working_depot):
+		self._log.info("client_handler", "ping == False")
+		if client_dict["completed"]:
+			self._log.info("client_handler", "completed == True")
+			if client_dict["successful"]:
+				self._log.info("client_handler", "successful == True")
+				depot_item = working_depot.get_by_id(client_dict["command_id"])
+				if depot_item is not None:
+					# sets the valus of depot item because the depot_item is completed
+					depot_item.set(client_dict["completed"], client_dict["stdout"], client_dict["exit_code"])
+					# print(f"Modified Depot Item: {depot_item.output()}")
+					self._server_info.update_last_successful_job(depot_item.output())
+					working_depot.count -= 1
+					self._log.info("client_handler", "depot item information updated")
+				else:
+					# is unable to obtain the depot_item by id
+					self._log.error("client_handler", "Could not find depot item by that id - returning: None, -1")
+					return None, -1
+			else:
+				#if unsequenced go onto the next item (table this one until the user has ruled on it), else wait for user responce
+				# The command was not successful
+				self._log.error("client_handler", "Command was not successful")
+		else:
+			# This should not happen
+			self._log.info("command_handler", "Command not completed")
 
 	def client_handler(self, client_dict, sender_key):
 		"""
@@ -49,35 +105,8 @@ class ClientHandler:
 		#gets working depot
 
 		if sender_key == self._server_public_key:
-			print("SERVER SENDER KEY")
-			if client_dict["type"] == "add":
-				self._command_handler.add_command_to_specified(client_dict["command"], client_dict["who"])
-				print("### - added to depot - ###")
-				print(self.depot_list.get_depot_list_info())
-				return "depot_item_added", -1
-				
-			if client_dict["type"] == "checkoff":
-				print("In checkout code")
-				self._command_handler.checkoff_command(client_dict["who"], client_dict["command_id"])
-				return "checked_off_depot_item", -1
-
-			if client_dict["type"] == "get_server_data":
-				print("Get Server Data")
-				self._server_info.update_depots_state(self.get_depots_data())
-				output = self._server_info.to_json()
-				print("--server_info--")
-				print(output)
-				return "get_server_data", output
+			return self._server_client_handler(client_dict, sender_key)
 			
-			if client_dict["type"] == "get_startup_data":
-				print("Get Startup Data")
-				json_static_endpoint_data = self._server_info.get_static_endpoint_data()
-				return "get_startup_data", json_static_endpoint_data
-
-
-			print("Recieved message from server, but 'type' is not 'add' or 'checkoff'.")
-
-			return None
 
 		self._server_info.update_heatbeat()
 		print("-------")
@@ -85,30 +114,7 @@ class ClientHandler:
 		working_depot = self.depot_list.get_working_depot(sender_key)
 
 		if client_dict["ping"] == False:
-			self._log.info("client_handler", "ping == False")
-			if client_dict["completed"]:
-				self._log.info("client_handler", "completed == True")
-				if client_dict["successful"]:
-					self._log.info("client_handler", "successful == True")
-					depot_item = working_depot.get_by_id(client_dict["command_id"])
-					if depot_item is not None:
-						# sets the valus of depot item because the depot_item is completed
-						depot_item.set(client_dict["completed"], client_dict["stdout"], client_dict["exit_code"])
-						# print(f"Modified Depot Item: {depot_item.output()}")
-						self._server_info.update_last_successful_job(depot_item.output())
-						working_depot.count -= 1
-						self._log.info("client_handler", "depot item information updated")
-					else:
-						# is unable to obtain the depot_item by id
-						self._log.error("client_handler", "Could not find depot item by that id - returning: None, -1")
-						return None, -1
-				else:
-					#if unsequenced go onto the next item (table this one until the user has ruled on it), else wait for user responce
-					# The command was not successful
-					self._log.error("client_handler", "Command was not successful")
-			else:
-				# This should not happen
-				self._log.info("command_handler", "Command not completed")
+			return self._client_ping(client_dict, working_depot)
 		else:
 			# Pinging the server to figure out if there is a command that can be picked up
 			self._log.info("command_handler", "Ping == True")
